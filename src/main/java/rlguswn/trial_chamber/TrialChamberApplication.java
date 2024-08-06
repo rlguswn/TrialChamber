8e325d0452eb6611164839007501d78071d5640a
주기현 <0318joo@naver.com>
2024-08-06 13:49:23 +0900

    프로젝트 생성

---
+++ src/main/java/rlguswn/trial_chamber/TrialChamberApplication.java
@@ -0,0 +1,13 @@
+package rlguswn.trial_chamber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialChamberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialChamberApplication.class, args);
+	}
+
+}
