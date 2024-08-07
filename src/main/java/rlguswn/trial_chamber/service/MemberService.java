f9f1d2229cd861fe4b86baaaaa48bb2e971380f1
주기현 <0318joo@naver.com>
2024-08-07 19:04:45 +0900

    회원 가입 서비스 구현

---
+++ src/main/java/rlguswn/trial_chamber/service/MemberService.java
@@ -0,0 +1,31 @@
+package rlguswn.trial_chamber.service;
+
+import jakarta.transaction.Transactional;
+import rlguswn.trial_chamber.domain.Member;
+import rlguswn.trial_chamber.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
