776ef0f7c6ae03b998f933b44a14eacb70ac3a52
주기현 <0318joo@naver.com>
2024-08-08 19:58:10 +0900

    로그인 폼 생성

---
+++ src/main/java/rlguswn/trial_chamber/controller/MemberLoginForm.java
@@ -0,0 +1,23 @@
+package rlguswn.trial_chamber.controller;
+
+public class MemberLoginForm {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
