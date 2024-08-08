3f78766e9a10a23ebb5bf0dedc9e4f9bf30f5fe2
주기현 <0318joo@naver.com>
2024-08-08 19:59:18 +0900

    임시 로그인 구현

--- src/main/java/rlguswn/trial_chamber/controller/MemberController.java
+++ src/main/java/rlguswn/trial_chamber/controller/MemberController.java
@@ -1,10 +1,13 @@
 package rlguswn.trial_chamber.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import rlguswn.trial_chamber.domain.Member;
 import rlguswn.trial_chamber.service.MemberService;
+
+import java.util.Optional;
 
 @Controller
 public class MemberController {
@@ -28,5 +31,21 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("members/login")
+    public String loginForm() {
+        return "members/loginMemberForm";
+    }
+
+    @PostMapping("/members/login")
+    public String login(MemberLoginForm form, Model model) {
+        Optional<Member> member = memberService.login(form.getUsername(), form.getPassword());
+        if (member.isPresent()) {
+            return "redirect:/";
+        } else {
+            model.addAttribute("error", "이름 혹은 비밀번호를 다시 한번 확인해주세요.");
+            return "members/loginMemberForm";
+        }
     }
 }
