d7da58625142fb0c16e1e3e8962dccbe6362302d
주기현 <0318joo@naver.com>
2024-09-03 18:57:30 +0900

    post domain dto 추가

---
+++ src/main/java/rlguswn/trial_chamber/dto/PostForm.java
@@ -0,0 +1,43 @@
+package rlguswn.trial_chamber.dto;
+
+import java.time.LocalDateTime;
+
+public class PostForm {
+
+    private String title;
+    private String description;
+    private LocalDateTime deadline;
+    private String previewImage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getPreviewImage() {
+        return previewImage;
+    }
+
+    public void setPreviewImage(String previewImage) {
+        this.previewImage = previewImage;
+    }
+}
