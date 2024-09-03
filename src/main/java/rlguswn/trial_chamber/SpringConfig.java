9d25ae3aadde9181cc43a519c20a444027b51f16
주기현 <0318joo@naver.com>
2024-09-03 18:59:04 +0900

    spring config에 post service, repository 추가

--- src/main/java/rlguswn/trial_chamber/SpringConfig.java
+++ src/main/java/rlguswn/trial_chamber/SpringConfig.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import rlguswn.trial_chamber.repository.JpaMemberRepository;
+import rlguswn.trial_chamber.repository.JpaPostRepository;
 import rlguswn.trial_chamber.repository.MemberRepository;
+import rlguswn.trial_chamber.repository.PostRepository;
 import rlguswn.trial_chamber.service.MemberService;
+import rlguswn.trial_chamber.service.PostService;
 
 @Configuration
 public class SpringConfig {
@@ -24,7 +27,17 @@ public class SpringConfig {
     }
 
     @Bean
+    public PostService postService() {
+        return new PostService(postRepository());
+    }
+
+    @Bean
     public MemberRepository memberRepository() {
         return new JpaMemberRepository(em);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new JpaPostRepository(em);
     }
 }
