42817a44e14fe64935c8115ce118dd49d8def69c
주기현 <0318joo@naver.com>
2024-09-03 18:58:46 +0900

    post controller 및 템플릿 생성

---
+++ src/main/java/rlguswn/trial_chamber/controller/PostController.java
@@ -0,0 +1,51 @@
+package rlguswn.trial_chamber.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import rlguswn.trial_chamber.domain.Member;
+import rlguswn.trial_chamber.domain.Post;
+import rlguswn.trial_chamber.dto.PostForm;
+import rlguswn.trial_chamber.service.MemberService;
+import rlguswn.trial_chamber.service.PostService;
+
+@Controller
+public class PostController {
+
+    private final PostService postService;
+    private final MemberService memberService;
+
+    @Autowired
+    public PostController(PostService postService, MemberService memberService, MemberService memberService1) {
+        this.postService = postService;
+        this.memberService = memberService1;
+    }
+
+    @GetMapping("/posts/new")
+    public String createPostForm() {
+        return "posts/createPostForm";
+    }
+
+    @PostMapping("/posts/new")
+    public String createPost(PostForm form) {
+        Post post = new Post(
+                form.getTitle(),
+                form.getDescription(),
+                form.getDeadline(),
+                form.getPreviewImage()
+        );
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        Member member = memberService.findByUsername(username);
+
+        post.setMember(member);
+
+        postService.createPost(post);
+        return "redirect:/";
+    }
+}
