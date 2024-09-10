8c352df4857f59681b1247041e8e5ce3fe779b43
주기현 <0318joo@naver.com>
2024-09-10 14:52:35 +0900

    에러가 발생했을 때 모두 같은 페이지로 리다이렉트 되는 문제를 수정

--- src/main/java/rlguswn/trial_chamber/controller/GlobalExceptionHandler.java
+++ src/main/java/rlguswn/trial_chamber/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package rlguswn.trial_chamber.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -8,14 +9,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
-    public String handleIllegalStateException(IllegalStateException ex, RedirectAttributes redirectAttributes) {
+    public String handleIllegalStateException(IllegalStateException ex, RedirectAttributes redirectAttributes, HttpServletRequest request) {
         redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
-        return "redirect:/members/new";
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException ex, RedirectAttributes redirectAttributes) {
+    public String handleIllegalArgumentException(IllegalArgumentException ex, RedirectAttributes redirectAttributes, HttpServletRequest request) {
         redirectAttributes.addFlashAttribute("errorMessage", ex.getMessage());
-        return "redirect:/members/new";
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 }
