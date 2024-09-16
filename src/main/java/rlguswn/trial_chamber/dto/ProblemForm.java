303fa16743672181ebad66659ee3472654b2d368
주기현 <0318joo@naver.com>
2024-09-16 21:25:52 +0900

    #5 problem 기능 구현하기 위한 dto 생성

---
+++ src/main/java/rlguswn/trial_chamber/dto/ProblemForm.java
@@ -0,0 +1,41 @@
+package rlguswn.trial_chamber.dto;
+
+public class ProblemForm {
+
+    private String title;
+    private String description;
+    private String trialType;
+    private Boolean autoGrade;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTrialType() {
+        return trialType;
+    }
+
+    public void setTrialType(String trialType) {
+        this.trialType = trialType;
+    }
+
+    public Boolean getAutoGrade() {
+        return autoGrade;
+    }
+
+    public void setAutoGrade(Boolean autoGrade) {
+        this.autoGrade = autoGrade;
+    }
+}
