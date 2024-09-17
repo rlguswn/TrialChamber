af464f6f6257b0f80c374f5d5a51921a0e8d668f
주기현 <0318joo@naver.com>
2024-09-17 18:23:07 +0900

    #5 submission 기능 구현을 위한 dto 생성

---
+++ src/main/java/rlguswn/trial_chamber/dto/SubmissionForm.java
@@ -0,0 +1,14 @@
+package rlguswn.trial_chamber.dto;
+
+public class SubmissionForm {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/rlguswn/trial_chamber/dto/SubmissionUpdateForm.java
@@ -0,0 +1,14 @@
+package rlguswn.trial_chamber.dto;
+
+public class SubmissionUpdateForm {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
