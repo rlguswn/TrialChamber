03c52be6cf939e82d22e61692d94086b06d99004
주기현 <0318joo@naver.com>
2024-09-23 20:31:27 +0900

    submission dto에 problem id 값 추가

--- src/main/java/rlguswn/trial_chamber/dto/SubmissionForm.java
+++ src/main/java/rlguswn/trial_chamber/dto/SubmissionForm.java
@@ -2,7 +2,16 @@ package rlguswn.trial_chamber.dto;
 
 public class SubmissionForm {
 
+    private Long problemId;
     private String content;
+
+    public Long getProblemId() {
+        return problemId;
+    }
+
+    public void setProblemId(Long problemId) {
+        this.problemId = problemId;
+    }
 
     public String getContent() {
         return content;
